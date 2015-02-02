29d10ee42c811b4ae712f75e0fe42d45fb04f83a
Jiang, Lei <jlnokia8850@gmail.com>
2015-02-02 10:22:42 +0800

    init

---
+++ test.cpp
@@ -0,0 +1,16 @@
+/* 
+* @Author: anchen
+* @Date:   2015-02-02 10:21:32
+* @Last Modified by:   anchen
+* @Last Modified time: 2015-02-02 10:22:31
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    pritnf("hello world!");
+    return 0;
+}
